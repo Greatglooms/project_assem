43274dac1ca0ba8e6deddab985b552434a68cbd1
Greatglooms <davletov.rustam@gmail.com>
2023-11-03 21:49:46 +0600

    add function devide for new blockb -> task#345

--- assem_app/views.py
+++ assem_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import RadioForm
-
+#def devide
 # tetstt
 def index(request):
     return HttpResponse("Hello, world! You're at the polls index.")
